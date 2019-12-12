0e49202cb24f8885d74c43f49c86d5fce9e16ff7
Pablo <p_felipe10@hotmail.com>
2019-12-12 21:54:22 -0200

    Inicializar repositório

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,5 @@
+class ApplicationController < ActionController::Base
+	def teste
+		render html: "Tchau,Brasil!"
+	end
+end
---
+++ config/routes.rb
@@ -0,0 +1,5 @@
+Rails.application.routes.draw do
+  root 'application#teste'
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
+
